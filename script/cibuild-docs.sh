612717899c55007f58868b6927ff7c407835ff8d
Gilles Hoarau <unicolored@gmail.com>
2025-04-21 18:24:56 +0200

    Update input path for Tailwind CSS in docs build script
    Renamed the input file for the Tailwind CSS build step to `_main.css` to align with the updated file structure. This ensures the documentation styles are built correctly without errors.

--- script/cibuild-docs.sh
+++ script/cibuild-docs.sh
@@ -4,7 +4,7 @@ set -e
 
 
 npx postcss src/liturgia-colors.css -o docs/styles/liturgia.css
-npx postcss src/tailwind/main.css -o docs/styles/liturgia.tailwind.css
+npx postcss src/tailwind/_main.css -o docs/styles/liturgia.tailwind.css
 
 yarn docs:build
 
